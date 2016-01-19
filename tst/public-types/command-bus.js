b66d60597f15d7362b4f56a46c501fb407c1baa9
Steve Gray <steve@Steves-MacBook.local>
2016-01-19 20:59:39 +1000

    CommandBus processCommand support

--- tst/public-types/command-bus.js
+++ tst/public-types/command-bus.js
@@ -10,7 +10,10 @@
 /* global it */
 const eventSauce = require('../../lib');
 const chai = require('chai');
+const chaiAsPromised = require('chai-as-promised');
+chai.use(chaiAsPromised);
 const expect = chai.expect;
+
 
 describe('CommandBus', () => {
   describe('Construction', () => {
@@ -56,6 +59,74 @@ describe('CommandBus', () => {
         expect(() => {
           instance.mapCommand(null, function test() {});
         }).to.throw(Error);
+      });
+    });
+
+    describe('processCommand', () => {
+      let latches = null;
+
+      // Wireup someCommand to deal with >= 0 accept
+      // < 0 = reject and not defined to be a non-promise
+      // response
+      beforeEach(() => {
+        latches = {};
+        instance.mapCommand('SomeCommand', (cmd) => {
+          if (cmd.latchIndex !== undefined) {
+            if (cmd.latchIndex >= 0) {
+              latches[cmd.latchIndex] = true;
+              return Promise.resolve(cmd.latchIndex);
+            }
+
+            latches[cmd.latchIndex] = false;
+            return Promise.reject(cmd.latchIndex);
+          }
+
+          if (cmd.explode) {
+            throw new Error('Boom');
+          }
+          return -1;
+        });
+      });
+
+      it('Should deal with succesful commands', () => {
+        return expect(instance.processCommand({
+          commandName: 'someCommand',
+          latchIndex: 42,
+        })).to.eventually.be.fulfilled();
+      });
+
+      it('Should deal with rejected commands', () => {
+        return expect(instance.processCommand({
+          commandName: 'someCommand',
+          latchIndex: -84,
+        })).to.eventually.be.rejected();
+      });
+
+      it('Should deal with non-promise result commands', () => {
+        return expect(instance.processCommand({
+          commandName: 'someCommand',
+        })).to.eventually.be.fulfilled();
+      });
+
+      it('Should reject promise generators that throw exceptions', () => {
+        return expect(instance.processCommand({
+          commandName: 'someCommand',
+          explode: true,
+        })).to.eventually.be.rejected();
+      });
+
+      it('Should throw for arguments not containing commandName', () => {
+        return expect(() => {
+          instance.processCommand({
+          invalidCommandName: 'someCommand',
+        })}).to.throw(Error);
+      });
+
+      it('Should throw for unmapped commandName', () => {
+        return expect(() => {
+          instance.processCommand({
+          commandName: 'noSuchCommand',
+        })}).to.throw(Error);
       });
     });
   });
